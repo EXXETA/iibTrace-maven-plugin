81b0dbba852aca40c0c53b7cbee22e69db679abf
unknown <thpo@EX213.exxeta-de.local>
2017-09-07 10:56:36 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,76 @@
+iibTrace-maven-plugin
+=================
+
+This open source plugin can be used to extract iib user traces during test execution.  
+
+
+## Install to local maven repository
+
+1. Install Maven
+2. Clone git repo
+3. Execute mvn install
+
+
+## Add plugin to pom.xml
+
+```xml
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>com.exxeta.we.iib</groupId>
+				<artifactId>iibTrace-maven-plugin</artifactId>
+				<version>1.0</version>
+				<executions>
+					<execution>
+						<id>pre-unit-test</id>
+						<goals>
+							<goal>prepare-trace</goal>
+						</goals>
+					</execution>
+					<execution>
+						<id>post-unit-test</id>
+						<phase>test</phase>
+						<goals>
+							<goal>report</goal>
+						</goals>
+					</execution>
+				</executions>
+				<configuration>
+					<mqsiProfile>${mqsi.dir}/mqsiprofile.cmd</mqsiProfile>
+					<mqsiReadLog>${mqsi.dir}/mqsireadlog.exe</mqsiReadLog>
+					<mqsiChangeTrace>${mqsi.dir}/mqsichangetrace.exe</mqsiChangeTrace>
+					<broker>TESTNODE</broker>
+					<executionGroup>EG_NAME</executionGroup>
+					<reportDir>${project.build.directory}/iibTrace</reportDir>
+				</configuration>
+			</plugin>
+		</plugins>
+	</build>
+```
+
+## Requirements
+
+- SonarQube 5.6
+
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## History
+
+- 1.0   - Initial release
+
+
+## Credits
+
+- EXXETA AG
+- See [Contributors](https://www.github.com/EXXETA/iibTrace-maven-plugin/graphs/contributors)
+
+## License
+
+Apache License v2.0
---
+++ src/main/java/com/exxeta/we/iib/trace/Report.java
@@ -0,0 +1,74 @@
+/*
+ * iibTrace maven plugin
+ * Copyright (C) 2017 Thomas Pohl and EXXETA AG
+ * http://www.exxeta.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exxeta.we.iib.trace;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "report")
+public class Report extends AbstractMojo {
+
+	@Parameter(required = true)
+	private String mqsiProfile;
+
+	@Parameter(required = true)
+	private String mqsiChangeTrace;
+
+	@Parameter(required = true)
+	private String mqsiReadLog;
+
+	@Parameter(required = true)
+	private String broker;
+
+	@Parameter(required = true)
+	private String executionGroup;
+
+	@Parameter(defaultValue = "${project.build.directory}/iibTrace")
+	private File reportDir;
+
+	@Parameter(defaultValue = "true")
+	private boolean clear;
+
+	public void execute() throws MojoExecutionException {
+		try {
+			getLog().info("Creating trace file. ");
+
+			String cmd = "cmd.exe /c call \"" + mqsiProfile.replace('/', '\\') + "\" 2>nul >nul && \""
+					+ mqsiReadLog.replace('/', '\\') + "\" " + broker + " -e " + executionGroup + " -u -o \""
+					+ reportDir.getAbsolutePath() + "/" + executionGroup + ".xml\" 2>nul";
+			getLog().info("Executing:" + cmd);
+			Runtime.getRuntime().exec(cmd).waitFor();
+
+			getLog().info("Resetting trace to none. ");
+
+			String level = "none";
+			cmd = "cmd.exe /c call \"" + mqsiProfile.replace('/', '\\') + "\" 2>nul >nul && \""
+					+ mqsiChangeTrace.replace('/', '\\') + "\" " + broker + " -u -e " + executionGroup + " -l " + level
+					+ " -c 2000 " + (clear ? " -r" : "");
+			getLog().info("Executing:" + cmd);
+			Runtime.getRuntime().exec(cmd).waitFor();
+
+		} catch (Exception e) {
+			getLog().error(e);
+		}
+	}
+}
